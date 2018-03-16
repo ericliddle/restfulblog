29a8cab5d14701dc8c63916623dd730e094eddaf
heyliddle <eric.liddle@gmail.com>
2018-03-16 00:53:12 -0700

    show page displays single post.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const bodyParser = require("body-parser");
 mongoose = require("mongoose"),
   express = require("express"),
-  app = express();
+  app = express(),
+  moment = require('moment');
 
 // App config      
 mongoose.connect("mongodb://localhost/restfulblog");
@@ -9,6 +10,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// var date = '2015-04-03';
+// var format = 'LLLL';
+// var result = moment(date).format(format);
 
 //Mongoose Model Config
 const blogSchema = new mongoose.Schema({
@@ -16,7 +20,8 @@ const blogSchema = new mongoose.Schema({
   image: String,
   // image: {type: String, default: "placeholderimg.jpg"},
   body: String,
-  created: { type: Date, default: Date.now() },
+  // created: { type: Date, default: Date.now() },
+  created: { type: String, default: moment()},
 });
 const Blog = mongoose.model("Blog", blogSchema);
 
@@ -55,12 +60,19 @@ app.post("/blogs", (req, res) => {
     } else {
       res.redirect("/blogs")
     }
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, blogPost) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: blogPost })
+    }
+    // res.send("Balls.")
   })
 })
-
-//   title: "Test blog",
-//   image: "https://images.unsplash.com/photo-1511468752539-facd2a2dff50?ixlib=rb-0.3.5&s=56b779b5d40051bfc60d8b59683d3664&auto=format&fit=crop&w=1350&q=80",
-//   body: "This is the test blog."
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.")
