74d914e630f5ea34d8850577d039ac7c2c4cb6b6
heyliddle <eric.liddle@gmail.com>
2018-03-17 19:00:23 -0700

    created delete route to remove blog post from db

--- app.js
+++ app.js
@@ -92,7 +92,16 @@ app.put("/blogs/:id", (req, res) => {
   })
 })
 
-
+// Delete
+app.delete("/blogs/:id", (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if(err) {
+      req.redirect("/blogs")
+    } else {
+      res.redirect("/blogs")
+    }
+  })
+})
 
 
 app.listen(3000, () => {
